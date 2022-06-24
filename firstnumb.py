0400d363f4a421478ad0df8929a8b224a2eabbd9
Siavash Soleimanpour <96922094+Siavash-Sol@users.noreply.github.com>
2022-06-24 04:56:10 -0700

    firstnumb in jadi pr

---
+++ firstnumb.py
@@ -0,0 +1,10 @@
+
+number = int(input('please input your number: '))
+tedad_maghsom = 0
+for i in range(1, number+1):
+    if number % i == 0:
+        tedad_maghsom += 1
+if tedad_maghsom == 2:
+    print('prime')
+else:
+    print('not prime')
